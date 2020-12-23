c32efed797b25cd02496104a811b4bb0c8299331
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-12-22 23:28:08 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -17,8 +17,6 @@ module.exports = class LoginRouter {
       return httpResponse.badRequest('password')
     }
     this.authUseCase.auth(email, password)
-    return {
-      statusCode: 401
-    }
+    return httpResponse.unauthorizedError()
   }
 }
