6616fd3313a437f5bcaa014b950dabf740c418f4
Reynaldo Layme <layme.reynaldo@icloud.com>
2021-01-01 19:06:49 -0300

    ensure Encrypter calls bcrypt with correct values

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,7 +1,11 @@
 module.exports = {
   isValid: true,
+  value: '',
+  hashed: '',
 
   async compare (value, hashed) {
+    this.value = value
+    this.hashed = hashed
     return this.isValid
   }
 }
