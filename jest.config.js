14b1dda8abade8d44a021aa1bb52283762676199
Reynaldo Layme <layme.reynaldo@icloud.com>
2021-01-02 21:46:14 -0300

    ignore module main in coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   watchPathIgnorePatterns: ['globalConfig'],
   preset: '@shelf/jest-mongodb'
 }
